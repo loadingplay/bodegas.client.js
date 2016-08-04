e1a8e46687502f20baa976df5d9ae59103e3a6f5
Estefi <estefany@loadingplay.com>
2016-08-04 12:02:45 -0400

    remove product

--- dist/bodegas.client.js
+++ dist/bodegas.client.js
@@ -1673,7 +1673,7 @@ ShoppingCartView.prototype.init = function()
     $(document).on('click', this.options.removeFromCart, function(evt)
     {
         evt.preventDefault();
-        self.removeProduct($(this));
+        self.removeProduct($(this)); //debe enviar id de producto
         self.render();
     });
 
