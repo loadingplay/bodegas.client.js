85aebd16c0867f964287755d2e9ee5ad3eb17519
Ricardo Silva <ricardo@loadingplay.com>
2017-01-31 11:29:23 -0300

    fix unittests

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,82 +1,103 @@
-'use strict';
 
-module.exports = function(grunt)  //jshint ignore: line
+(function()
 {
-    /*** istanbul ***/
-    grunt.loadNpmTasks('grunt-contrib-qunit');
-    // grunt.loadNpmTasks('grunt-qunit-istanbul');
-    /*** istanbul ***/
+    'use strict';
 
-    require('load-grunt-tasks')(grunt);  //jshint ignore: line
-
-    grunt.initConfig({
-
+    module.exports = function(grunt)  //jshint ignore: line
+    {
+        var connect_port = 3001;
         /*** istanbul ***/
-        pkg: grunt.file.readJSON('package.json'),
-
-        qunit : {
-            all: ['tests/index.html'],
-            options: {
-                coverage: {
-                  src: ['dist/bodegas.client.js'],
-                  instrumentedFiles: 'temp/',
-                  htmlReport: 'report/coverage',
-                  coberturaReport: 'report/',
-                  linesThresholdPct: 20
-                }
-            }
-        },
+        grunt.loadNpmTasks('grunt-contrib-qunit');
+        // grunt.loadNpmTasks('grunt-qunit-istanbul');
         /*** istanbul ***/
 
-        concat: {
-            js: {
-                src: ['src/**/*.js'],
-                dest: 'dist/bodegas.client.js',
-            },
-            css: {
-                src : ['css/**/*.css'],
-                dest: 'dist/bodegas.client.css',
+        require('load-grunt-tasks')(grunt);  //jshint ignore: line
 
-            }
-        },
+        grunt.initConfig({
 
-        watch: {
-            js: {
-                files: ['src/**/*.js'],
-                tasks: ['concat']
-            },
-            css: {
-                files: ['css/**/*.css'],
-                tasks: ['concat']
-            }
-        },
+            /*** istanbul ***/
+            pkg: grunt.file.readJSON('package.json'),
 
-        uglify: {
-            my_target: {
-                files: {
-                    'dist/bodegas.client.js': ['src/**/*.js']
+            qunit : {
+                all: {
+                    options: {
+                        urls: ['http://localhost:'+connect_port+'/tests/index.html']
+                    }
                 }
-            }
-        },
 
-        express : {
-            all : {
-                options: {
-                    port : 9000,
-                    hostname : 'localhost',
-                    bases : ['.']
+                // options: {
+                //     coverage: {
+                //       src: ['dist/bodegas.client.js'],
+                //       instrumentedFiles: 'temp/',
+                //       htmlReport: 'report/coverage',
+                //       coberturaReport: 'report/',
+                //       linesThresholdPct: 20
+                //     }
+                // }
+            },
+
+            connect: {
+                server: {
+                    options: {
+                        port: connect_port,
+                        base: '.'
+                    }
                 }
-            }
-        },
+            },
 
-    });
+            /*** istanbul ***/
 
-    grunt.loadNpmTasks('grunt-contrib-concat');
-    grunt.loadNpmTasks('grunt-contrib-watch');
-    grunt.loadNpmTasks('grunt-contrib-uglify');
-    grunt.loadNpmTasks('grunt-express');
+            concat: {
+                js: {
+                    src: ['src/**/*.js'],
+                    dest: 'dist/bodegas.client.js',
+                },
+                css: {
+                    src : ['css/**/*.css'],
+                    dest: 'dist/bodegas.client.css',
 
-    grunt.registerTask('tests', ['qunit']);
-    grunt.registerTask('build', ['concat', 'uglify']);
-    grunt.registerTask('default', ['concat', 'uglify', 'express', 'watch']);
-};
+                }
+            },
+
+            watch: {
+                js: {
+                    files: ['src/**/*.js'],
+                    tasks: ['concat']
+                },
+                css: {
+                    files: ['css/**/*.css'],
+                    tasks: ['concat']
+                }
+            },
+
+            uglify: {
+                my_target: {
+                    files: {
+                        'dist/bodegas.client.js': ['src/**/*.js']
+                    }
+                }
+            },
+
+            express : {
+                all : {
+                    options: {
+                        port : 9000,
+                        hostname : 'localhost',
+                        bases : ['.']
+                    }
+                }
+            },
+
+        });
+
+        grunt.loadNpmTasks('grunt-contrib-concat');
+        grunt.loadNpmTasks('grunt-contrib-watch');
+        grunt.loadNpmTasks('grunt-contrib-uglify');
+        grunt.loadNpmTasks('grunt-express');
+        grunt.loadNpmTasks('grunt-contrib-connect');
+
+        grunt.registerTask('tests', ['connect', 'qunit']);
+        grunt.registerTask('build', ['concat', 'uglify']);
+        grunt.registerTask('default', ['concat', 'uglify', 'express', 'watch']);
+    };
+})();
