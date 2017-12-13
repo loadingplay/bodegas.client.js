fa8dd5b72de6c907be01dc11fb361b63cddbcfeb
blur <jose.silvap@alumnos.duoc.cl>
2017-12-13 10:41:47 -0300

    Added support for random in column of config

--- src/product.js
+++ src/product.js
@@ -62,6 +62,17 @@ Product.prototype._list = function(page, items_per_page, ignore_stock, callback_
         term = search_query;
     }
 
+    if (column === "random") // Check for random in sorting column
+    {
+        if ($.cookie('shopping-cart')) // Check that shopping-cart cookie is created
+        {
+            column = "random(" + $.cookie('shopping-cart') + ")";
+        }else
+        {
+            column = "random(000000-000-000-000-000000)"; // Default value for random and the cookie doesn't exists or can't be retrieved
+        }
+    }
+
     //@todo: Add validation for correct spelling of column sort word.
 
     jQuery.post(Utils.getURLWithoutParam('product/search'),
