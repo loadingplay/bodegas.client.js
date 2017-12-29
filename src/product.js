4485af2de5bb8212500bedee4770ca0fca43216c
blur <jose.silvap@alumnos.duoc.cl>
2017-12-29 12:51:29 -0300

    travis

--- src/product.js
+++ src/product.js
@@ -33,6 +33,8 @@ Product.prototype.get = function(product_id, user_or_callback, callback)
         });
 };
 
+var random_seed; // random seed for random sorting of products
+
 Product.prototype._list = function(page, items_per_page, ignore_stock, callback_or_tags, search_query, user, operator, column, direction, callback)
 {
     var tags = 'false';
@@ -64,17 +66,10 @@ Product.prototype._list = function(page, items_per_page, ignore_stock, callback_
 
     if (column === "random") // Check for random in sorting column
     {
-        if ($.cookie('shopping-cart')) // Check that shopping-cart cookie is created
-        {
-            column = "random(" + $.cookie('shopping-cart') + ")";
-        }
-        else
-        {
-            column = "random(000000-000-000-000-000000)"; // Default value for random if the cookie doesn't exists or can't be read
-        }
+        if(page===1)
+            random_seed = Math.random();
+        column = "random("+random_seed+")"; //The column param must be sent as random(some_random_number) or else API won't recognize it
     }
-
-    //@todo: Add validation for correct spelling of column sort word.
 
     jQuery.post(Utils.getURLWithoutParam('product/search'),
         {
@@ -95,11 +90,15 @@ Product.prototype._list = function(page, items_per_page, ignore_stock, callback_
             if (data.products !== undefined)
             {
                 product_list = data.products;
+
+                if(page===1)
+                    $('.products').empty();
             }
 
             callback(product_list);
         });
 };
+
 
 
 Product.prototype.destroy = function()
